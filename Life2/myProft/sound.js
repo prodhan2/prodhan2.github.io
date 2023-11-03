ab267d7480457aa5032f783993d07d0709686508
Sujan prodhan <113437122+prodhan2@users.noreply.github.com>
2023-11-03 13:13:18 +0600

    Add files via upload

---
+++ Life2/myProft/sound.js
@@ -0,0 +1,8 @@
+const audio = new Audio("https://www.fesliyanstudios.com/play-mp3/387");
+const buttons = document.querySelectorAll(".sound");
+
+buttons.forEach(button => {
+  button.addEventListener("click", () => {
+    audio.play();
+  });
+});
