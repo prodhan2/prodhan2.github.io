2c5e226a41ebaa9a764722e6250dfd2c9f49d3ec
Sujan prodhan <113437122+prodhan2@users.noreply.github.com>
2023-11-03 13:02:44 +0600

    Update portfolioSOund.js

--- audio/portfolioSOund.js
+++ audio/portfolioSOund.js
@@ -1,6 +1,6 @@
 
 const audio = new Audio("https://www.fesliyanstudios.com/play-mp3/387");
-const buttons = document.querySelectorAll(".menu-btn");
+const buttons = document.querySelectorAll(".sound");
 
 buttons.forEach(button => {
   button.addEventListener("click", () => {
